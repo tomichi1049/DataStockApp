a65585be357daeb7ff6d511bf9f042423f3cfd60
tomichi1049 <tomitachisato8025@gmail.com>
2023-12-23 13:23:15 +0900

    2023/12/23 13:23 commit

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/',[PostController::class, 'index']);
-
+Route::get('/posts/create', [PostController::class, 'create']);
+Route::post('/posts', [PostController::class, 'store']);
 Route::get('/posts/{post}', [PostController::class ,'show']);
 
