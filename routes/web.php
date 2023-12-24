051d487c9e0c6f8d3e457d41984dc19ccebd2457
tomichi1049 <tomitachisato8025@gmail.com>
2023-12-24 17:38:59 +0900

    2023/12/24 17:38 commit

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,5 @@ Route::post('/posts', [PostController::class, 'store']);
 Route::get('/posts/{post}', [PostController::class ,'show']);
 Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
 Route::put('/posts/{post}', [PostController::class, 'update']);
+Route::delete('/posts/{post}', [PostController::class,'delete']);
 
