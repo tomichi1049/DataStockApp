0290d30193fb35f935cacecd058d5a3e56b77bc7
tomichi1049 <tomitachisato8025@gmail.com>
2024-01-01 00:08:17 +0900

    2024/01/01 0:08 commit

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -22,4 +22,11 @@ class Post extends Model
         // updated_atで降順に並べたあと、limitで件数制限をかける
         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
+    
+    public function getLists()
+    {
+        $categories = Post::pluck('category_name', 'id');
+
+        return $categories;
+    }
 }
--- database/migrations/2023_12_31_135704_create_categories_table.php
+++ database/migrations/2023_12_31_135704_create_categories_table.php
@@ -13,18 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('title',50);
-            $table->string('category',50);
-            $table->string('user',50);
-            $table->string('text',2000);
-            $table->string('image_url')->nullable();
-            $table->timestamps();
+            $table->string('category_name',255);
         });
     }
-    
-
 
     /**
      * Reverse the migrations.
@@ -33,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('categories');
     }
 };
